aa286c2ed8aba8528b8898f71e678385068a8f88
Datasciencer <103309402+datasciencer2022@users.noreply.github.com>
2025-04-02 14:24:53 -0300

    Create periodo.php

---
+++ periodo.php
@@ -0,0 +1,19 @@
+<html>
+<?php 
+
+	$dtIni = strtotime($_GET['dtIni']);
+	$dtFim = strtotime($_GET['dtFim']);
+	$dias = resolve($dtIni, $dtFim);
+?>
+	<head>
+		<title>Cálculo da Soma de 2 Números</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<center>
+			<h1>Entre <?php echo(date("d/m/Y",$dtIni)) ;?> e <?php echo(date("d/m/Y",$dtFim))?></h1>
+			<h2> Tem <?php echo ("$dias"); ?> dias</h2>
+		</center>
+	</body>
+</html>
